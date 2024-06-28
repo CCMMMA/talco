Fixed concentration calculation

app/utils.py
@@ -1,6 +1,7 @@
 from config import Config
 from shapely.geometry import Point
 import netCDF4
+import numpy as np
 
 
 def get_index_lat_long(min_lat, max_lat, min_long, max_long):
@@ -17,9 +18,8 @@ def getConc(url, index_min_lat, index_min_long, index_max_lat, index_max_long):
         dataset = netCDF4.Dataset(url)
         lat_indices = range(index_min_lat - 2, index_max_lat + 2)
         lon_indices = range(index_min_long - 2, index_max_long + 2)
-        concentration = dataset['conc'][0, 0, lat_indices, lon_indices]
-
-        value = sum(sum(concentration))
+        concentration = dataset['conc'][0, :, lat_indices, lon_indices]
+        value = np.sum(np.ma.filled(concentration, fill_value=0))
     except Exception as e:
         print(e)
         value = -100